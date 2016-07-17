6df5f519dd4040259a1a0be1ad91cffa0ca8ed96
Stanislav Gurin <work.gurin@gmail.com>
2016-07-17 21:58:34 +0300

    Revert "support-crud"
    This reverts commit 1e062b6bde273836c44db62ca4d5d682ab45f032.

--- support-crud/src/main/java/com/ox/repository/TopicRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ox.repository;
-
-import com.ox.domain.Topic;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, Long> {
-
-}
